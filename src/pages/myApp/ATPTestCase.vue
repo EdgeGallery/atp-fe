f146ceec4743e2881a147357e4aa73a469d29bf9
baizhenzhen <850084376@qq.com>
2020-11-16 14:42:09 +0800

    联调修改接口传参

--- src/pages/myApp/ATPTestCase.vue
+++ src/pages/myApp/ATPTestCase.vue
@@ -271,6 +271,7 @@ export default {
       let fd = new FormData()
       let packageForm = this.packageForm
       fd.append('file', packageForm.fileList[0])
+      console.log(fd)
       this.dialogVisible = true
       Atp.getDependencyApi(fd).then(res => {
         // let data = res.data.dependencydata.dependency   mock
@@ -302,7 +303,11 @@ export default {
     ConfirmTest () {
       let fd = new FormData()
       let packageForm = this.packageForm
-      fd.append('file', packageForm.fileList)
+      packageForm.fileList.forEach(item => {
+        fd.append('file', item)
+      })
+      // fd.append('file', packageForm.fileList)
+      console.log(fd)
       Atp.creatTaskApi(fd).then(res => {
         let taskId = res.data[0].id
         sessionStorage.setItem('taskId', taskId)
