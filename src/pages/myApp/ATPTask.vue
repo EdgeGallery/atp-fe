25bcc11cf16bde56444921c9d9f4d74be9429849
baizhenzhen <850084376@qq.com>
2021-03-03 22:40:24 +0800

    嵌套页面显示国际化

--- src/pages/myApp/ATPTask.vue
+++ src/pages/myApp/ATPTask.vue
@@ -229,10 +229,12 @@ export default {
       dataLoading: true,
       currentData: [],
       userId: sessionStorage.getItem('userId'),
-      userName: sessionStorage.getItem('userName')
+      userName: sessionStorage.getItem('userName'),
+      language: ''
     }
   },
   mounted () {
+    this.getLanguage()
     this.getTaskList()
     this.interval = setInterval(() => {
       this.getTaskList()
@@ -248,6 +250,15 @@ export default {
     next()
   },
   methods: {
+    getLanguage () {
+      let currUrl = window.location.href
+      if (currUrl.indexOf('?') !== -1) {
+        this.language = currUrl.split('?')[1].split('=')[1]
+      }
+      localStorage.setItem('language', this.language)
+      this.$i18n.locale = this.language
+      this.$store.commit('changeLaguage', { language: this.language })
+    },
     getCurrentPageData (val) {
       this.currentData = val
     },
