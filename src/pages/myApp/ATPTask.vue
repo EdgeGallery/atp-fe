3d4da33dc9cbb109f9e366d7d62b6251a983a753
baizhenzhen <850084376@qq.com>
2021-03-17 22:07:21 +0800

    修改问题单

--- src/pages/myApp/ATPTask.vue
+++ src/pages/myApp/ATPTask.vue
@@ -280,7 +280,7 @@ export default {
     handleClickTaskNo (val) {
       let taskId = val.id
       sessionStorage.setItem('taskId', taskId)
-      this.$router.push('/atpprocess')
+      this.$router.push({ name: 'atpprocess', query: { taskId: this.taskId } })
     },
     contrastTime () {
       if (this.form.endTime && this.form.createTime > this.form.endTime) {
